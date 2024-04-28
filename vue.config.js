7142bd55fc1d3b46af16ffdc978c91c400786f7e
Crbnboy <kaikaiming@outlook.com>
2024-04-28 15:48:34 +0800

    config整理（vue-cli最后版本）

--- vue.config.js
+++ vue.config.js
@@ -6,40 +6,39 @@ module.exports = defineConfig({
 //为避免跨域问题,访问api和后端服务器要走代理
 module.exports = {
   devServer: {
-      open: true,
-      host: '0.0.0.0',
-      port: 8080,
-      allowedHosts: 'all',
-      proxy: {
-          '/kugou': {
-              target: 'https://m.kugou.com',
-              changOrigin: true,
-              pathRewrite: {
-                  '^/kugou': ''
-              }
-          },
-          '/mobilecdngz-kugou':{
-            target:'http://mobilecdngz.kugou.com',
-            changOrigin: true,
-            pathRewrite: {
-                '^/mobilecdngz-kugou': ''
-            }
-          },
-          '/flask':{
-            target:'localhost:5000',
-            changOrigin: true,
-            pathRewrite: {
-                '^/flask': ''
-            }
-          },
-          '/host':{
-            target:'http://192.168.43.101:5000',
-            changOrigin: true,
-            pathRewrite: {
-                '^/host': ''
-            }
-          }
-          
+    open: true,
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: 'all',
+    proxy: {
+      '/kugou': {
+        target: 'https://m.kugou.com',
+        changOrigin: true,
+        pathRewrite: {
+          '^/kugou': ''
+        }
+      },
+      '/mobilecdngz-kugou': {
+        target: 'http://mobilecdngz.kugou.com',
+        changOrigin: true,
+        pathRewrite: {
+          '^/mobilecdngz-kugou': ''
+        }
+      },
+      '/flask': {
+        target: 'localhost:5000',
+        changOrigin: true,
+        pathRewrite: {
+          '^/flask': ''
+        }
+      },
+      '/host': {
+        target: 'http://127.0.0.1:5000',
+        changOrigin: true,
+        pathRewrite: {
+          '^/host': ''
+        }
       }
+    }
   }
 }
